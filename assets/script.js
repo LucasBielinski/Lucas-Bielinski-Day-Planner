c89fe0ddeb85913ba44570d5a48a4b2954b09e38
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-12-08 18:43:18 -0500

    deleted unecessary text

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,6 @@ var tasks = [{hour: '8'},{hour: 9}, {hour: 10}, {hour: 11}, {hour: 12}, {hour: 1
 
 $(function () {
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage? 
-  //
 if (hours < currentTime){
   timeArea.attr({"class": "past"})
 } else if (hours === currentTime){
@@ -23,15 +16,6 @@ if (hours < currentTime){
   timeArea.attr({'class': 'future'})
 }
 
-
-  
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
   function calltasks() {
     var tasks = localStorage.getItem("description")
     if (tasks) {
