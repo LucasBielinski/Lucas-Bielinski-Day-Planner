c316ccc318979a0aa869eba88a96e9c467bc503f
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-12-09 18:08:13 -0500

    fix if statements add load function

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,67 @@
-var save = $('#sbmit')
-var descInput = $('desc')
-var timeArea = $('time-block')
+var save = $('.submit')
+var textAreas = $('.desc')
+var timeRows = $('.time-block')
 var hours = ($(this).parent().attr("data-hour"))
+console.log(hours)
 var currentTime = dayjs().format('HH')
-var tasks = [{hour: '8'},{hour: '9'}, {hour: '10'}, {hour: '11'}, {hour: '12'}, {hour: '1'}, {hour: '2'}, {hour: '3'}, {hour: '4'}, {hour: '5'}] 
+var tasks = [{hour: '8', name:""},{hour: '9', name:""}, {hour: '10', name:""}, {hour: '11', name:""}, {hour: '12', name: ""}, {hour: '1', name:''}, {hour: '2', name: ''}, {hour: '3', name:''}, {hour: '4', name:''}, {hour: '5', name:''}] 
 
 
-$(function () {
 
-if (hours < currentTime){
-  timeArea.attr({"class": "past"})
-} else if (hours === currentTime){
-  timeArea.attr({'class':'present'})
-} else if (hours > currentTime){
-  timeArea.attr({'class': 'future'})
-}
 
-  function calltasks() {
-    var tasks = localStorage.getItem("description")
-    if (tasks) {
-      tasks = JSON.parse(tasks)
-    } else {
-      tasks = []
-    }
-  }
+  // function calltasks() {
+  //   var tasks = localStorage.getItem("description")
+  //   if (tasks) {
+  //     tasks = JSON.parse(tasks)
+  //   } else {
+  //     tasks = []
+  //   }
+  // }
   
-  function savetasks() {
-    var description = {
-      hour:($(this).parent().attr("data-hour")),
-      name:($(this).prev(".desc").val())
-    }
-    for (i=0; i < tasks.length; i++) {
-      if (tasks[i].hour === description.hour){
-        tasks[i] = description
-        break
-      }
-    }
-  localStorage.setItem("description", JSON.stringify(tasks))
-}
+ 
 
-  save.on('click', savetasks); 
 
   var today = dayjs().format('dddd, DD, YYYY')
   $('#currentDay').text('today is ' +today+ ' !')
-});
 
+
+function savetasks(event) {
+  event.preventDefault()
+  console.log(event)
+  console.log("Saving tasks")
+  console.log(this)
+  var description = {
+    hour:($(this).parent().attr("data-hour")),
+    name:($(this).prev().val())
+  }
+  console.log(description)
+  for (i=0; i < tasks.length; i++) {
+    if (tasks[i].hour === description.hour){
+      tasks[i] = description
+     break
+    }
+
+  }
+  console.log(tasks)
+  localStorage.setItem("description", JSON.stringify(tasks))
+}
+
+for(let i=0; i< save.length; i++ ){
+  // console.log(save[i])
+  save[i].addEventListener('click', savetasks); 
+}
+
+console.log(currentTime)
+var savedTask = JSON.parse(localStorage.getItem("description"))
+for(let i=0; i< textAreas.length; i++){
+  var nodeHour = i+8
+  if (nodeHour < currentTime){
+    textAreas[i].classList.add("past")
+  } else if (nodeHour === currentTime){
+    textAreas[i].classList.add("present")
+  } else if (nodeHour > currentTime){
+    textAreas[i].classList.add("future")
+  }
+  
+textAreas[i].value = savedTask[i].name
+}
